707fa01d06056a4d563f937861f8fe27b8cbf036
Elizabeth Phippen <phippene@seattleu.edu>
2017-10-22 13:22:27 -0700

    display menu options outline

--- Source/main.c
+++ Source/main.c
@@ -342,8 +342,6 @@ void display(void* data) {
   /*
   TODO:
     add field to data for Menu Mode vs Annunciate Mode vs menu options
-    print statements for menu options
-    print statements for heart rate, blood pressure, & temp
   */
   
   /*  
@@ -352,12 +350,10 @@ void display(void* data) {
   
     //  Blood Pressure hover
     if( ? ){
-      print("* Blood Pressure        ", 0, 1);
+      print("* Blood Pressure", 0, 1);
     }else{
-      print("  Blood Pressure        ", 0, 1);
+      print("  Blood Pressure", 0, 1);
     }
-  
-    print("              ", 0, 2);  // clear row
   
     //  Temperature hover
     if( ? ){
@@ -375,31 +371,48 @@ void display(void* data) {
   }
   */
   
-  /*  Heart Rate Mode
+  /*
+  if( ? ){  // Menu option display
+    print("Menu", 0, 0);
+    
+    if( ? ){  // Blood Pressure
+      print("Blood Pressure", 0, 1);
+      volatile int t = *((Display*)data)->sysPress;
+      intPrint(*((Display*)data)->sysPress, 3, 0, 2);         //Systolic: should never be over 3 char
+      print("/", 3, 2);
+      intPrint(*((Display*)data)->diaPress, 5, 4, 2);         //Diastolic: should never be over 5 char
+      print("mm Hg", 9, 2);
+    }
   
-  */
-  
-  /*  Blood Pressure Mode
-  
-  */
-  
-  /*  Temp Mode
-  
+    if( ? ){  // temperature
+      print("Temperature", 0, 1);
+      fPrint(*((Display*)data)->temp, 4, 0, 2);               //Temperature: should never be over 4 char
+      print("C", 4, 2);
+    }
+    
+    if( ? ){  // heart rate
+      print("Heart Rate", 0, 1);
+      intPrint(*((Display*)data)->heartRate, 3, 0, 2);        //Heartrate: should never be over 3 char
+      print("BPM", 3, 2);
+    }
+  }
   */
 
-  /*    Annunciate Mode
-	volatile int t = *((Display*)data)->sysPress;
-	intPrint(*((Display*)data)->sysPress, 3, 0, 0);         //Systolic: should never be over 3 char
-	print("/", 3, 0);
-	intPrint(*((Display*)data)->diaPress, 5, 4, 0);         //Diastolic: should never be over 5 char
-	print("mm Hg", 9, 0);
+  /*    
+  if( ? ){  // Annunciate Mode
+    volatile int t = *((Display*)data)->sysPress;
+    intPrint(*((Display*)data)->sysPress, 3, 0, 0);         //Systolic: should never be over 3 char
+    print("/", 3, 0);
+    intPrint(*((Display*)data)->diaPress, 5, 4, 0);         //Diastolic: should never be over 5 char
+    print("mm Hg", 9, 0);
 
-	fPrint(*((Display*)data)->temp, 4, 0, 1);               //Temperature: should never be over 4 char
-	print("C", 4, 1);
-	intPrint(*((Display*)data)->heartRate, 3, 6, 1);        //Heartrate: should never be over 3 char
-	print("BPM", 9, 1);
+    fPrint(*((Display*)data)->temp, 4, 0, 1);               //Temperature: should never be over 4 char
+    print("C", 4, 1);
+    intPrint(*((Display*)data)->heartRate, 3, 6, 1);        //Heartrate: should never be over 3 char
+    print("BPM", 9, 1);
 
-	intPrint(*((Display*)data)->batteryState, 3, 13, 1);    //battery: should never be over 3 char
+    intPrint(*((Display*)data)->batteryState, 3, 13, 1);    //battery: should never be over 3 char
+  }
   */
 
 }
